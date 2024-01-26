22df9ae848a4d9c2d1e501ddf3a0e3c4fcf7153c
CHOGANGYEOL <rkdduf06@icloud.com>
2024-01-26 17:41:36 +0900

    organize:24.01.26 study

--- project/src/index.ts
+++ project/src/index.ts
@@ -1,5 +1,25 @@
-import { exit, init } from "./myPackage";
+import crypto from "crypto";
 
-init({ debug: true, url: "" });
-exit(1);
-const hello = () => "hi";
+interface BlockShape {
+  hash: string;
+  prevHash: string;
+  height: number;
+  data: string;
+}
+
+class Block implements BlockShape {
+  public hash: string;
+  constructor(
+    public prevHash: string,
+    public height: number,
+    public data: string
+  ) {
+    // block의 hash값은 prevHash, height, data값을 이용해 계산되어야한다.
+    // hash는 고유 서명과 같은것이다.
+    this.hash = Block.calculateHash(prevHash, height, data);
+  }
+  static calculateHash(prevHash: string, height: number, data: string): string {
+    const toHash = `${prevHash}${height}${data}`;
+    return crypto.createHash("sha256").update(toHash).digest("hex");
+  }
+}
